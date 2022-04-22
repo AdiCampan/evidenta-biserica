626f5ba3c1dcf8cd1438235ae6aae331c635f1ca
Flaviu Cimpan <fcimpan@macadamian.com>
2022-04-22 14:38:13 +0200

    automatic refetching

--- evidenta-biserica/src/services/members.js
+++ evidenta-biserica/src/services/members.js
@@ -3,23 +3,26 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const membersApi = createApi({
   reducerPath: 'membersApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://evidenta-biserica.herokuapp.com/' }),
-  tagTypes: [],
+  tagTypes: ['members'],
   endpoints: (builder) => ({
     getMembers: builder.query({
       query: () => `members/`,
+      providesTags: ['members'],
     }),
     addMember: builder.mutation({
         query: (body) => ({
             url: 'members/',
             method: 'POST',
             body: body,
-        }),
+      }),
+      invalidatesTags: ['members'],
     }),
     delMember: builder.mutation({
         query: (id) => ({
             url: `members/${id}`,
             method: 'DELETE',
-        }),
+      }),
+      invalidatesTags: ['members'],
     })
   }),
 });
